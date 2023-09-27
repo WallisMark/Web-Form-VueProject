4a12792a4ace44f313bcf419eaed40ea80b9f87a
wallis <wallismark@gmail.com>
2023-09-27 21:06:47 +0200

    accept user inputs

--- src/components/SingupForm.vue
+++ src/components/SingupForm.vue
@@ -1,13 +1,40 @@
 <template>
     <form>
         <label>email:</label>
-        <input type="email" required>
-    </form>
+        <input type="email" required v-model="email">
+        <label>Password:</label>
+        <input type="password" required v-model="password">
+        <label>Role:</label>
+        <select v-model="role">
+            <option value = "front-end" >Front-End</option>
+            <option value = "back-end" >Back-End</option>
+            <option value = "full-stack" >Full-Stack</option>
+        </select>
+
+    
+    <div class="terms">
+        <input type="checkbox" required v-model="checked">
+        <label>Accept terms and conditions</label>
+    </div>
+</form>
+<p>your email was: {{ email }}</p>
+    <p>your password was: {{ password }}</p>
+    <p>your password was: {{ role }}</p>
+    <p>Terms and conditions: {{ checked }}</p>
+
   
 </template>
 
 <script>
 export default {
+    data(){
+        return{
+            email: "",
+            password:"",
+            role:"front-end",
+            checked:false
+        }
+    }
 
 }
 </script>
@@ -30,7 +57,7 @@ label{
     letter-spacing: 1px;
     font-weight: bold;
 }
-input{
+input, select{
     display: block;
     padding: 10px 6px;
     width:100%;
@@ -38,6 +65,13 @@ input{
     border: none;
     border-bottom: 1px solid #ddd;
     color:#555;
+}
+input[type="checkbox"]{
+    display: inline-block;
+    width: 16px;
+    margin: 0 10px 0 0;
+    position:relative;
+    top:2px;
 }
 
 </style>
